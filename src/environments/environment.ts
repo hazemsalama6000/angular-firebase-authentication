a8a7085636f68928a91ab701baef41c2a9642541
hazem.salama6000 <hazem.salama6000@gmail.com>
2022-08-17 14:14:00 +0200

    firebase initialize

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'chat-10250',
+    appId: '1:684753066503:web:67d369f6f16d4f412428a2',
+    storageBucket: 'chat-10250.appspot.com',
+    apiKey: 'AIzaSyCAYjVhObTe559PSb7IU2-N5cbmFYDIWq8',
+    authDomain: 'chat-10250.firebaseapp.com',
+    messagingSenderId: '684753066503',
+    measurementId: 'G-J33VKTBX99',
+  },
   production: false
 };
 
