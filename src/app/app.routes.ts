e1f89bff0cbb62f83bcab2b54d7aadbbc712b2ad
Your Name <hazem.salama60@yahoo.com>
2022-09-08 05:05:27 +0200

    redirect without path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 
 
 export const routes:Routes=[
+  {path:'',redirectTo:'auth/login',pathMatch:'full'},
   {path:'auth',loadChildren : ()=> import('./feature-moduels/auth/auth.module').then(a=>a.AuthModule)},
   {path:'layout',loadChildren:()=>import('./feature-moduels/layout/layout.module').then(a=>a.LayoutModule)}
 ];
